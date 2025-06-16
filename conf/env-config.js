ab683b4a364ad7d949d6f4e6a74431f0554fd81d
marcin-sieradzki-maersk <marcin.sieradzki@maersk.com>
2025-06-16 15:05:44 +0200

    Add GitHub Actions workflow for deployment to GitHub Pages and create environment configuration file for build process

---
+++ conf/env-config.js
@@ -0,0 +1,10 @@
+// This file will be generated during the build process
+// with values from GitHub Secrets
+window.envConfig = {
+    domain: 'audiocodes.com',
+    addresses: [],
+    caller: 'Anonymous',
+    callerDN: 'Anonymous',
+    call: '+15164614104',
+    credential: 'auth-token'
+}; 
