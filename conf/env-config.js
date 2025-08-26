ad94ed4bb8bf0745dfd84f0d1b674201c8195d5e
marcin-sieradzki-maersk <93589871+marcin-sieradzki-maersk@users.noreply.github.com>
2025-08-26 05:38:28 +0000

    Deploying to gh-pages from @ marcin-sieradzki-maersk/WebRTC-mdsy@4f2ef0b0e235dd12b7c71449c850b2b5b4e77860 🚀

--- conf/env-config.js
+++
@@ -1,8 +0,0 @@
-window.envConfig = {
-  domain: 'audiocodes.com',
-  addresses: ['wss://your-sbc-server.com:10081'],
-  caller: 'Anonymous',
-  callerDN: 'Anonymous',
-  call: '+15164614104',
-  credential: 'auth-token'
-};
